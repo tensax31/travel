b3355a780d62d13bdae6ad4c42ecf4ea2705bd7a
Regi Alston <tensax31@gmail.com>
2018-07-01 18:42:07 -0500

    Added browser support fallbacks

--- app/assets/scripts/Vendor.js
+++ app/assets/scripts/Vendor.js
@@ -1,1 +1,3 @@
-import 'lazysizes'
+import '../../temp/scripts/modernizr'
+import 'picturefill' //Responsive Images in Legacy Browsers
+import 'lazysizes'  // Support "lazy" loading of images for faster page loads
